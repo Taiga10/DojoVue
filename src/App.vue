506850497246e1b70b868c9fe6aad5b4df1d6ac8
Taiga10 <fernanda.fujosh1@gmail.com>
2019-07-13 12:16:58 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>Traductor</h1>
+    <translateIn v-on:formSubmit="traducirTexto"></translateIn>
+    <translateOut v-text="traduciresteTexto"></translateOut>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import translateIn from "./components/translateIn.vue";
+import translateOut from "./components/translateOut";
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    translateIn,
+    translateOut
+  },
+  data: function(){
+    return{
+      traduciresteTexto:''
+    }
+  },
+  methods: {
+    traducirTexto: function(texto, idioma) {
+      this.$http.get(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190622T213455Z.f67e95598c7aee64.c0c49d0594aa527178d83c8e3c1a81ed05709a56&lang=${idioma}&text=${texto}`)
+      .then((response)=>{
+        this.traduciresteTexto = response.body.text[0];
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/translateOut.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="translateOut">
+        <h2>{{traduciresteTexto}}</h2>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'translateOut',
+    props: ['traduciresteTexto']
+}
+</script>
+
+<style>
+</style>
